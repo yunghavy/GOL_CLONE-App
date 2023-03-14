e9c83cdedbddbaf0b4e6e983533592576c4382f3
kimcipher <kimutaij192@gmail.com>
2023-03-14 08:26:27 +0300

    Global

---
+++ GlobalStyles.js
@@ -0,0 +1,44 @@
+/* fonts */
+export const FontFamily = {
+  interSemibold: "Inter_semibold",
+  uI16Medium: "Inter_medium",
+  interBold: "Inter_bold",
+  ubuntuBold: "Ubuntu_bold",
+  interRegular: "Inter_regular",
+  openSansBold: "Open Sans_bold",
+  openSansSemibold: "Open Sans_semibold",
+  ubuntuLight: "Ubuntu_light",
+  interExtrabold: "Inter_extrabold",
+};
+/* font sizes */
+export const FontSize = {
+  size_xl: 20,
+  size_base: 16,
+  size_mini: 15,
+  size_xs: 12,
+  size_5xl: 24,
+  size_7xs: 6,
+  size_sm: 14,
+};
+/* Colors */
+export const Color = {
+  white1: "#fff",
+  orangered_100: "#ff3d00",
+  greenPrimary: "#5db075",
+  gray03: "#bdbdbd",
+  black: "#000",
+  whitesmoke_100: "#efefef",
+  whitesmoke_200: "#e9e9e9",
+  gray021: "#e8e8e8",
+  gray_100: "#818181",
+  dimgray_100: "#6f6f6f",
+  darkslategray: "#353333",
+  teal: "#197c65",
+};
+/* border radiuses */
+export const Border = {
+  br_xs: 12,
+  br_5xs: 8,
+  br_81xl: 100,
+  br_base: 16,
+};
